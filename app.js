2359b9b8ecb047df94723f43704a40067fb9d9cc
Shubham_Baghel <shubhambaghel34@gmail.com>
2023-03-27 11:47:59 +0530

    updated code with messages

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const redis = require('redis');
 
-// Create a client and connect to Redis.
+// Create a client which will connect to Redis server.
 const client = redis.createClient({
   host: '127.0.0.1',
   port: 6000,
-  // password: 'password',
 });
 
-// Run a Redis command, receive response in callback.
-client.set('hello', 'world', (err, reply) => {
-  console.log(reply); // OK
 
-  // Run a second Redis command now we know that the
-  // first one completed.  Again, response in callback.
-  client.get('hello', (getErr, getReply) => {
-    console.log(getReply); // world
+client.set('name', 'devtoUser', (err, response) => {
+  console.log(response); // This will return OK as result
 
-    // Quit client and free up resources.
+  
+  client.get('name', (err, getResponse) => {
+    console.log(getResponse); // devToUser
+
+    // close client
     client.quit();
   });
 });
